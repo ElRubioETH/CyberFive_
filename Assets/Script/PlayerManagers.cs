016912c4b4dca748d25a7ec8808370becbe54131
ElRubioETH <henkenmanybruh@gmail.com>
2024-06-28 17:44:43 +0700

    hehe

--- Assets/Script/PlayerManagers.cs
+++ Assets/Script/PlayerManagers.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI; // Required for using UI elements
 using System.Collections;
 
 public class PlayerController : MonoBehaviour
@@ -8,21 +9,35 @@ public class PlayerController : MonoBehaviour
     public Transform groundCheck;
     public LayerMask groundLayer;
     public Animator animator;
+    public Slider healthBar; // Reference to the UI Slider for the health bar
 
     private Rigidbody2D rb;
     private SpriteRenderer spriteRenderer;
     private bool isGrounded;
     private bool isAttacking;
+    private int maxHealth = 100;
+    private int currentHealth;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        currentHealth = maxHealth;
+        UpdateHealthBar();
     }
 
     void Update()
     {
-        float moveInput = Input.GetAxis("Horizontal");
+        float moveInput = 0;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveInput = -1;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            moveInput = 1;
+        }
 
         if (!isAttacking)
         {
@@ -71,5 +86,30 @@ public class PlayerController : MonoBehaviour
         yield return new WaitForSeconds(0.5f); // Adjust based on attack animation length
         animator.SetBool("IsAttacking", false);
         isAttacking = false;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        UpdateHealthBar();
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void UpdateHealthBar()
+    {
+        if (healthBar != null)
+        {
+            healthBar.value = (float)currentHealth / maxHealth;
+        }
+    }
+
+    void Die()
+    {
+        // Handle player death (e.g., play animation, reload level, etc.)
+        animator.SetTrigger("Die");
+        // Additional death logic here
     }
 }
