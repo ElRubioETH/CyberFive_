037045efa55d5b48b9e1217befd7d17cf45876e7
ElRubioETH <henkenmanybruh@gmail.com>
2024-07-11 18:11:08 +0700

    hehe

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -1,10 +1,33 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
     public int health = 100;
+    public int maxHealth = 100; // Maximum health for the slider
     public int goldReward = 100; // Gold given when the enemy dies
     public int damageFromProjectile = 50; // Damage taken from player projectiles
+    public GameObject healthBarPrefab; // Prefab of the health bar
+    private Slider healthSlider;
+    private Transform canvasTransform;
+
+    private void Start()
+    {
+        canvasTransform = GameObject.Find("Canvas").transform;
+        GameObject healthBar = Instantiate(healthBarPrefab, canvasTransform);
+        healthSlider = healthBar.GetComponent<Slider>();
+        healthSlider.maxValue = maxHealth;
+        healthSlider.value = health;
+    }
+
+    private void Update()
+    {
+        if (healthSlider != null)
+        {
+            Vector3 healthBarPosition = Camera.main.WorldToScreenPoint(transform.position + new Vector3(0, 1, 0));
+            healthSlider.transform.position = healthBarPosition;
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -26,6 +49,10 @@ public class Enemy : MonoBehaviour
     public void TakeDamage(int damage)
     {
         health -= damage;
+        if (healthSlider != null)
+        {
+            healthSlider.value = health;
+        }
         if (health <= 0)
         {
             Die();
@@ -39,6 +66,7 @@ public class Enemy : MonoBehaviour
         {
             playerController.AddGold(goldReward);
         }
+        Destroy(healthSlider.gameObject);
         Destroy(gameObject);
     }
 }
