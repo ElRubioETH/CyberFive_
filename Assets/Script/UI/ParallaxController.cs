8aa64898299e598aee6e17ece12e14e8d36747b5
duyyyyyyyyy <duytnps41276@gmail.com>
2024-08-13 15:15:11 +0700

    duy
    duy

--- Assets/Script/UI/ParallaxController.cs
+++ Assets/Script/UI/ParallaxController.cs
@@ -1,18 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class ParallaxControler : MonoBehaviour
+public class ParallaxController : MonoBehaviour
 {
-    Transform cam;
+    Transform cam; //Main Camera
     Vector3 camStartPos;
-    float distance;
+    float distance; //jarak antara start camera posisi dan current posisi
 
     GameObject[] backgrounds;
     Material[] mat;
     float[] backSpeed;
+
+    float farthestBack;
+
     [Range(0.01f, 0.05f)]
-    public float farthestBack;
     public float parallaxSpeed;
 
+    // Start is called before the first frame update
     void Start()
     {
         cam = Camera.main.transform;
@@ -28,20 +33,21 @@ public class ParallaxControler : MonoBehaviour
             backgrounds[i] = transform.GetChild(i).gameObject;
             mat[i] = backgrounds[i].GetComponent<Renderer>().material;
         }
+
         BackSpeedCalculate(backCount);
     }
 
     void BackSpeedCalculate(int backCount)
     {
-        for (int i = 0; i < backCount; i++)
+        for (int i = 0; i < backCount; i++) //find the farthest background
         {
-            if ((backgrounds[i].transform.position.z - camStartPos.z) > farthestBack)
+            if ((backgrounds[i].transform.position.z - cam.position.z) > farthestBack)
             {
                 farthestBack = backgrounds[i].transform.position.z - cam.position.z;
             }
         }
 
-        for (int i = 0; i < backCount; i++)
+        for (int i = 0; i < backCount; i++) //set the speed of bacground
         {
             backSpeed[i] = 1 - (backgrounds[i].transform.position.z - cam.position.z) / farthestBack;
         }
@@ -50,15 +56,8 @@ public class ParallaxControler : MonoBehaviour
     private void LateUpdate()
     {
         distance = cam.position.x - camStartPos.x;
+        transform.position = new Vector3(cam.position.x, transform.position.y, 0);
 
-        // Fix the Y position of the camera so that it always stays centered vertically
-        Vector3 newCamPos = new Vector3(cam.position.x, camStartPos.y, cam.position.z);
-        cam.position = newCamPos;
-
-        // Update the transform position to follow the camera's X position only
-        transform.position = new Vector3(cam.position.x, camStartPos.y, transform.position.z);
-
-        // Update the parallax effect for each background
         for (int i = 0; i < backgrounds.Length; i++)
         {
             float speed = backSpeed[i] * parallaxSpeed;
@@ -66,3 +65,5 @@ public class ParallaxControler : MonoBehaviour
         }
     }
 }
+
+
